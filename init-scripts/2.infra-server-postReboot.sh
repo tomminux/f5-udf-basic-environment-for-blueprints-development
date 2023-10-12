52e3e1ed509107431f8bc6dd56ec2ed034cbb6cb
Paolo Arcagni <tomminux@users.noreply.github.com>
2023-10-12 16:51:24 +0200

    Update 2.infra-server-postReboot.sh

--- init-scripts/2.infra-server-postReboot.sh
+++ init-scripts/2.infra-server-postReboot.sh
@@ -27,8 +27,10 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 ## ..:: Docker Community Edition installation ::..
 ## ----------------------------------------------------------------------------
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce
 sudo usermod -aG docker ubuntu
